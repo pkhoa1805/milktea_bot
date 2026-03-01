dcb6c0102f8005127c9d24a1d1c1266c66ccecab
PhamKhoa180524 <phamkhoa180524@gmail.com>
2026-03-01 14:25:10 +0700

    Initial commit - Spring Boot Telegram Bot

---
+++ src/main/java/com/khoa/milktea_bot/MilkteaBotApplication.java
@@ -0,0 +1,12 @@
+package com.khoa.milktea_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkteaBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilkteaBotApplication.class, args);
+	}
+}
---
+++ src/test/java/com/khoa/milktea_bot/MilkteaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khoa.milktea_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkteaBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
